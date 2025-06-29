a614b66d1a1218cf8e55fcfd2bbcea4bf41e2c8d
erenaydin0 <erenaydin.esa@gmail.com>
2025-06-29 04:46:22 +0300

    CSS yapısı artık çok daha modüler ve yönetilebilir hale geld

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/tokens.css';
 import './styles/main.css';
 import './styles/toast.css';
 import App from './App';
